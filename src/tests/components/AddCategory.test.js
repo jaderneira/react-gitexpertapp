633532876149c0c5ba5522cb17936dd02aa6ec6e
Jader Neira <jader.neira@gmail.com>
2021-08-10 17:19:37 -0500

    ADD: should change the text input

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -6,10 +6,18 @@ describe('Tests in <AddCategory />', () => {
 
     const setCategories = () => {}
     const wrapper = shallow(<AddCategory setCategories={ setCategories }/>);
-    
-    
+        
     test('should show <AddCategory /> correctly', () => {
         expect(wrapper).toMatchSnapshot();
     })
+
+    test('should change the text input', () => {
+        const input = wrapper.find('input');
+        const value = 'Hola mundo'
+        
+        input.simulate('change', { target: { value } });
+        
+    })
+    
     
 })
